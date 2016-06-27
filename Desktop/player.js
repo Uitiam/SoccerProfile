6e83b21893a083bfcb0eb8bc912a9bd6686ca70a
Uitiam <maitiumorton@gmail.com>
2016-06-27 13:32:24 -0700

    Add files via upload

---
+++ Desktop/player.js
@@ -0,0 +1,13 @@
+//user Class sets and creates user
+function Player(n, t) {
+    var name = n;
+    var team = t;
+
+    this.getName = funtion() {
+        return name;
+    }
+
+    this.getTeam = fucntion() {
+        return team;
+    }
+}
--- Desktop/user.js
+++ Desktop/user.js
@@ -1,1 +1,14 @@
-//user object
+//user Class sets and creates user
+function User(e, t) {
+    var email = e;
+    var type = t;
+
+
+    this.getEmail = function() {
+        return email;
+    }
+
+    this.getType = function() {
+        return type;
+    }    
+}
